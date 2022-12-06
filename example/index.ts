24de1517e5db67224562b0ba2849fb20c60eb5d1
Ethan Teng <1345118870@qq.com>
2022-12-06 22:16:20 +0800

    [test] add example

--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,43 @@
 import { Editor } from '../lib/index';
 import '../lib/asset/index.css';
+import { Block } from '../lib/virtualNode/block';
+import { NodeTypes } from '../lib/virtualNode/nodeTypes';
+import { h } from '../lib/virtualNode/h';
+import { keydownHandler } from '../lib/virtualNode/events/keydownHandler';
+import { EventName } from '../lib/virtualNode/events/eventNames';
+
+const blockNode1 = new Block(
+  h(
+    NodeTypes.BLOCK,
+    { contentEditable: 'true' },
+    [h(NodeTypes.BOLD | NodeTypes.PLAIN_TEXT, {}, 'hello world1')],
+    [[EventName.KEYDOWN, keydownHandler, false]]
+  )
+);
+const blockNode2 = new Block(
+  h(
+    NodeTypes.BLOCK,
+    {},
+    [h(NodeTypes.BOLD | NodeTypes.PLAIN_TEXT, {}, 'hello world2')],
+    [[EventName.KEYDOWN, keydownHandler, false]]
+  )
+);
+const blockNode3 = new Block(
+  h(
+    NodeTypes.BLOCK,
+    {},
+    [h(NodeTypes.BOLD | NodeTypes.PLAIN_TEXT, {}, 'hello world3')],
+    [[EventName.KEYDOWN, keydownHandler, false]]
+  )
+);
+const blockNode4 = new Block(
+  h(
+    NodeTypes.BLOCK,
+    {},
+    [h(NodeTypes.BOLD | NodeTypes.PLAIN_TEXT, {}, 'hello world4')],
+    [[EventName.KEYDOWN, keydownHandler, false]]
+  )
+);
 
 const e = new Editor({ container: '#editor' });
-e.init();
+e.init([blockNode1, blockNode2, blockNode3, blockNode4]);
