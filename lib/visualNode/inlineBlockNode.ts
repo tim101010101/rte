998596db8f9e99150aad1efc2ef317420ff92806
Ethan Teng <1345118870@qq.com>
2022-12-07 21:15:57 +0800

    Delete lib/virsualNode directory

--- lib/visualNode/inlineBlockNode.ts
+++
@@ -1,8 +0,0 @@
-import { NodeType } from './nodeType';
-import { BasicNode } from './basicNode';
-
-export class InlineBlockNode extends BasicNode {
-  constructor(type: NodeType, dom: HTMLElement, content: string) {
-    super(type, dom, content);
-  }
-}
