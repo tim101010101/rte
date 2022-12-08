c7c449d6b614fd3aa713c8b03d892c5c5531c258
Ethan Teng <1345118870@qq.com>
2022-12-08 20:46:41 +0800

    [feat] parser

---
+++ lib/render/parser.ts
@@ -0,0 +1,6 @@
+import { Token } from '../types/token';
+import { VirtualNode } from '../types/virtualNode';
+
+export const parser = (tokens: Array<Token>): VirtualNode => {
+  // TODO
+};
--- lib/virtualNode/nodeTypes.ts
+++
@@ -1,6 +0,0 @@
-export enum NodeTypes {
-  BLOCK = 1,
-
-  PLAIN_TEXT = 1 << 1,
-  BOLD = 1 << 2,
-}
