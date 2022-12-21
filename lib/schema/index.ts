c98159618065915f374005c24e1e0c4789bc71a0
Ethan Teng <1345118870@qq.com>
2022-12-21 14:19:49 +0800

    [feat] init class schema

--- lib/schema/index.ts
+++ lib/schema/index.ts
@@ -1,1 +1,31 @@
+import { s, t } from 'lib/model';
+import { NodeType, TagName, ClassName } from 'lib/static';
+
+const { BOLD, PLAIN_TEXT } = NodeType;
+const { SPAN } = TagName;
+const { RTE_PLAIN_TEXT, RTE_BOLD } = ClassName;
+
+const config = {
+  schema: [
+    {
+      key: 'bold',
+      marker: {
+        prefix: '**',
+        suffix: '**',
+      },
+      render(text: string) {
+        return s(BOLD, SPAN, { classList: [RTE_BOLD] }, [
+          t(PLAIN_TEXT, SPAN, { classList: [RTE_PLAIN_TEXT] }, text),
+        ]);
+      },
+    },
+  ],
+};
+
+export class Schema {
+  constructor() {
+    // TODO
+  }
+}
+
 export * from './parser';
