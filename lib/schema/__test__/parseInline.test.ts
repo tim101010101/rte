85be4f86e182c9ced3cbd408a16f5d4298e9a9f8
Ethan Teng <1345118870@qq.com>
2022-12-25 21:33:21 +0800

    [test] unit tests of inline parser

--- lib/schema/__test__/parseInline.test.ts
+++ lib/schema/__test__/parseInline.test.ts
@@ -1,17 +1,21 @@
-import { SchemaConfig, SyntaxNode, VirtualNode } from 'lib/types';
+import { SchemaConfig, VirtualNode } from 'lib/types';
 import { isTextNode, s, t } from 'lib/model';
-import { NodeType, ClassName, TagName } from 'lib/static';
+import { NodeType, TagName } from 'lib/static';
 import { parseInline } from '../parser/index';
 
-const { LINE, PLAIN_TEXT, BOLD, EM } = NodeType;
+const { LINE, BOLD, EM } = NodeType;
 const { DIV, SPAN } = TagName;
+
+const syntax = (type: NodeType, tagName: TagName, children: any, marker: any) =>
+  s(type, tagName, {}, children, [], marker);
+const text = (text: string) => t(SPAN, {}, text);
 
 const inline: SchemaConfig['inline'] = {
   bold: {
     reg: /(?<prefix>\*\*|__)(?=\S)(?<content>[\s\S]+?)(\\*)\k<prefix>(?!(\*|_))/,
     render(groups, children) {
       const { prefix } = groups;
-      return s(BOLD, SPAN, {}, children, [], {
+      return syntax(BOLD, SPAN, children, {
         prefix,
         suffix: prefix,
       });
@@ -21,7 +25,7 @@ const inline: SchemaConfig['inline'] = {
     reg: /(?<prefix>\*|_)(?=\S)(?<content>[\s\S]+?)(\\*)\k<prefix>(?!\k<prefix>)/,
     render(groups, children) {
       const { prefix } = groups;
-      return s(EM, SPAN, {}, children, [], {
+      return syntax(EM, SPAN, children, {
         prefix,
         suffix: prefix,
       });
@@ -31,7 +35,7 @@ const inline: SchemaConfig['inline'] = {
     reg: /(`{1})([^`]+?|.{2,})\1/,
     render(groups, children) {
       const { prefix } = groups;
-      return s(EM, SPAN, {}, children, [], {
+      return syntax(EM, SPAN, children, {
         prefix,
         suffix: prefix,
       });
@@ -39,32 +43,80 @@ const inline: SchemaConfig['inline'] = {
   },
 };
 
-const text = (text: string) => t(SPAN, {}, text);
-
 describe('parseInline', () => {
-  const generator = (root: VirtualNode): string => {
-    if (!root) return '';
+  const parse = (src: string) => parseInline(src, inline, text);
+  const ws = () => text(' ');
+  const fooBold = (marker: string) =>
+    syntax(BOLD, SPAN, [text('foo')], { prefix: marker, suffix: marker });
+  const fooEm = (marker: string) =>
+    syntax(EM, SPAN, [text('foo')], { prefix: marker, suffix: marker });
 
-    if (isTextNode(root)) {
-      return root.text;
-    } else {
-      let subRes = '';
-      const { marker, children } = root;
-      const { prefix, suffix } = marker;
-      if (prefix) subRes += prefix;
-      subRes += children.reduce((content, cur) => {
-        content += generator(cur);
-        return content;
-      }, '');
-      if (suffix) subRes += suffix;
+  test('handle marker correctly', () => {
+    expect(parse('**foo**')).toStrictEqual([fooBold('**')]);
+    expect(parse('__foo__')).toStrictEqual([fooBold('__')]);
+    expect(parse('*foo*')).toStrictEqual([fooEm('*')]);
+    expect(parse('_foo_')).toStrictEqual([fooEm('_')]);
+  });
 
-      return subRes;
-    }
-  };
+  test('hendle plain text', () => {
+    expect(parse(' ')).toStrictEqual([ws()]);
+    expect(parse(' foo')).toStrictEqual([text(' foo')]);
+    expect(parse('foo ')).toStrictEqual([text('foo ')]);
+    expect(parse(' foo ')).toStrictEqual([text(' foo ')]);
+    expect(parse(' **foo**')).toStrictEqual([ws(), fooBold('**')]);
+    expect(parse('**foo** ')).toStrictEqual([fooBold('**'), ws()]);
+    expect(parse(' **foo** ')).toStrictEqual([ws(), fooBold('**'), ws()]);
+  });
+
+  test('hande nested syntax node', () => {
+    expect(parse('**_foo_**')).toStrictEqual([
+      syntax(
+        BOLD,
+        SPAN,
+        [syntax(EM, SPAN, [text('foo')], { prefix: '_', suffix: '_' })],
+        {
+          prefix: '**',
+          suffix: '**',
+        }
+      ),
+    ]);
+    expect(parse('**_foo_ bar**')).toStrictEqual([
+      syntax(
+        BOLD,
+        SPAN,
+        [
+          syntax(EM, SPAN, [text('foo')], { prefix: '_', suffix: '_' }),
+          text(' bar'),
+        ],
+        {
+          prefix: '**',
+          suffix: '**',
+        }
+      ),
+    ]);
+  });
 
   test('smoke test', () => {
+    const generator = (root: VirtualNode): string => {
+      if (!root) return '';
+
+      if (isTextNode(root)) {
+        return root.text;
+      } else {
+        let subRes = '';
+        const { marker, children } = root;
+        const { prefix, suffix } = marker;
+        if (prefix) subRes += prefix;
+        subRes += children.reduce((content, cur) => {
+          content += generator(cur);
+          return content;
+        }, '');
+        if (suffix) subRes += suffix;
+
+        return subRes;
+      }
+    };
     const src = '__This__ *is* **Hello _World_** yes **i_t_** _is_';
-    const children = parseInline(src, inline, text);
-    expect(generator(s(LINE, DIV, {}, children))).toStrictEqual(src);
+    expect(generator(syntax(LINE, DIV, parse(src), {}))).toStrictEqual(src);
   });
 });
