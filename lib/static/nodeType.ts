a21115f77f1bf5e3456983c30c348d70457f1977
Ethan Teng <1345118870@qq.com>
2022-12-26 14:50:49 +0800

    [feat] add some enumerated values

--- lib/static/nodeType.ts
+++ lib/static/nodeType.ts
@@ -2,8 +2,10 @@ export enum NodeType {
   LINE = 1,
   PLAIN_TEXT = 1 << 1,
   BOLD = 1 << 2,
-  EM = 1 << 3,
+  ITALIC = 1 << 3,
   PREFIX = 1 << 4,
   SUFFIX = 1 << 5,
-  HEADING = 1 << 6,
+
+  HEADING = 1 << 10,
+  DIVIDE = 1 << 11,
 }
