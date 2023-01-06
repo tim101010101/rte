93f890da7d04eff7e852a3c024467b61441804ea
Ethan Teng <1345118870@qq.com>
2023-01-06 19:52:19 +0800

    [feat] util function and unit test

---
+++ lib/utils/__test__/string.test.ts
@@ -0,0 +1,20 @@
+import { insertAt, removeAt } from 'lib/utils';
+
+describe('string', () => {
+  describe('insertAt', () => {
+    test('smoke', () => {
+      expect(insertAt(' world', 0, 'hello')).toBe('hello world');
+    });
+  });
+
+  describe('removeAt', () => {
+    test('smoke', () => {
+      expect(removeAt('hello world', 0)).toBe('ello world');
+      expect(removeAt('hello world', 10)).toBe('hello worl');
+      expect(removeAt('hello world', 5)).toBe('helloworld');
+
+      expect(removeAt('hello world', 0, 6)).toBe('world');
+      expect(removeAt('hello world', 5, 6)).toBe('hello');
+    });
+  });
+});
--- lib/utils/string.ts
+++ lib/utils/string.ts
@@ -1,3 +1,7 @@
 export const insertAt = (source: string, pos: number, newStr: string) => {
   return `${source.slice(0, pos)}${newStr}${source.slice(pos)}`;
 };
+
+export const removeAt = (source: string, pos: number, count: number = 1) => {
+  return `${source.slice(0, pos)}${source.slice(pos + count)}`;
+};
