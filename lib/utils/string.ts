d65e63eb113ac307e7c346143499a252b318db69
Ethan Teng <1345118870@qq.com>
2023-01-07 14:12:05 +0800

    [perf] improve performance of concat

--- lib/utils/string.ts
+++ lib/utils/string.ts
@@ -6,6 +6,6 @@ export const removeAt = (source: string, pos: number, count: number = 1) => {
   return `${source.slice(0, pos)}${source.slice(pos + count)}`;
 };
 
-export const concat = (source: string, ...sources: Array<string>): string => {
-  return source.concat(...sources);
+export const concat = (...sources: Array<string>): string => {
+  return sources.join('');
 };
